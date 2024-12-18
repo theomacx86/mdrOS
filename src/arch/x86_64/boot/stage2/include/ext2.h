c2fd891f5be398586c3b5a4b6ab9e8f6a528c015
Théo <44744715+theomacx86@users.noreply.github.com>
2024-12-18 10:37:31 +0100

    EXT2 Inode structure

--- src/arch/x86_64/boot/stage2/include/ext2.h
+++ src/arch/x86_64/boot/stage2/include/ext2.h
@@ -66,6 +66,38 @@ typedef struct ext2_block_group_descriptor
     uint16_t directories_count;
 } __attribute__((packed)) ext2_block_group_descriptor_s;
 
+typedef struct ext2_inode {
+    uint16_t type_and_permissions; // 0-1: Type and Permissions
+    uint16_t user_id;              // 2-3: User ID
+    uint32_t size_lower;           // 4-7: Lower 32 bits of size in bytes
+    uint32_t access_time;          // 8-11: Last Access Time (POSIX time)
+    uint32_t creation_time;        // 12-15: Creation Time (POSIX time)
+    uint32_t modification_time;    // 16-19: Last Modification time (POSIX time)
+    uint32_t deletion_time;        // 20-23: Deletion time (POSIX time)
+    uint16_t group_id;             // 24-25: Group ID
+    uint16_t hard_link_count;      // 26-27: Count of hard links
+    uint32_t sector_count;         // 28-31: Count of disk sectors
+    uint32_t flags;                // 32-35: Flags
+    uint32_t os_specific_1;        // 36-39: OS Specific Value #1
+    uint32_t direct_block[12];     // 40-87: Direct Block Pointers 0-11
+    uint32_t singly_indirect;      // 88-91: Singly Indirect Block Pointer
+    uint32_t doubly_indirect;      // 92-95: Doubly Indirect Block Pointer
+    uint32_t triply_indirect;      // 96-99: Triply Indirect Block Pointer
+    uint32_t generation_number;    // 100-103: Generation number
+    uint32_t extended_attribute;   // 104-107: Extended attribute block
+    uint32_t upper_file_size;      // 108-111: Upper 32 bits of file size or Directory ACL
+    uint32_t fragment_block;       // 112-115: Block address of fragment
+    uint8_t os_specific_2[12];     // 116-127: OS Specific Value #2
+} __attribute__((packed)) ext2_inode_s;
+
+#define INODE_TYPE_FIFO         0x1000
+#define INODE_TYPE_CHARDEV      0x2000
+#define INODE_TYPE_DIRECTORY    0x4000
+#define INODE_TYPE_BLOCKDEV     0x6000
+#define INODE_TYPE_FILE         0x8000
+#define INODE_TYPE_SYMLINK      0xA000
+#define INODE_TYPE_SOCKET       0xC000
+
 void ext2_init();
 
 uint32_t ext2_get_block_size();
